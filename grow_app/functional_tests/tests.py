62266875d721e9da613f8aa473b75216a1054edd
Michael Wheeler <wheeler.m@husky.neu.edu>
2019-01-13 12:48:02 -0500

    Created project structure for first sprint.

---
+++ grow_app/functional_tests/tests.py
@@ -0,0 +1,9 @@
+"""
+This file contains functional tests, meant to test the behavior of the system from the outside.
+"""
+
+from django.test import LiveServerTestCase
+
+
+class BasicInventoryInteractionsTest(LiveServerTestCase):
+    pass
