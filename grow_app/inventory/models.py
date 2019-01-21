c9b95ebe0a9e118ea4ce44281a044cf5b925dece
Michael Wheeler <wheeler.m@husky.neu.edu>
2019-01-20 20:17:31 -0500

    Clean: Fixed up the model file formatting

--- grow_app/inventory/models.py
+++ grow_app/inventory/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 
 
+class Variety(models.Model):
+    """Represents the types of plants that can be grown. Has a name and number of days between plant and harvest."""
+    name = models.CharField(max_length=50)
+    days_plant_to_harvest = models.IntegerField()
+
+
 class Crop(models.Model):
     """Represents a single attempt to grow a tray of Microgreens at a given time. Maintains a history of growth data
     in the form of sensor data, lifecycle advancements, tray movements, grower actions, and free-form notes.
@@ -10,7 +16,7 @@ class Crop(models.Model):
     -> History recorded elsewhere, but linked by the crop's ID.
     """
 
-    variety = models.ForeignKey(Variety)
+    variety = models.ForeignKey(Variety, on_delete=models.PROTECT)
     days_germ = models.IntegerField()
     days_grow = models.IntegerField()
     harvest_in_house = models.BooleanField(default=False)
@@ -36,6 +42,7 @@ class CropRecord(models.Model):
     record_type = models.CharField(max_length=10, choices=RECORD_TYPES)
     note = models.CharField(max_length=200)
 
+
 class Location(models.Model):
     """Represents the physical location of a Tray. Can be a Slot in-house, or Customer upon delivery."""
     LOCATION_TYPES = (
@@ -45,9 +52,11 @@ class Location(models.Model):
     name = models.CharField(max_length=60)
     location_type = models.CharField(max_length=4, choices=LOCATION_TYPES)
 
+
 class Order(models.Model):
     """Represents a Customer's order, with details necessary for generating Crops and tasks. Implement later."""
     pass
+
 
 class Tray(models.Model):
     """Represents the container in which a crop is grown. Has a barcode, current Crop, and Location."""
@@ -56,12 +65,8 @@ class Tray(models.Model):
         ('1010', '10x10"'),
         ('0505', '5x5"'),
     )
-    barcode = models.charField(max_length=50)
+    barcode = models.CharField(max_length=50)
     current_crop = models.OneToOneField(Crop, on_delete=models.DO_NOTHING, blank=True, null=True)
     size = models.CharField(max_length=4, choices=TRAY_SIZES, default='1010')
-    location = models.ForeignKey(Location)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
 
-class Variety(models.Model):
-    """Represents the types of plants that can be grown. Has a name and number of days between plant and harvest."""
-    name = models.CharField(max_length=50)
-    days_plant_to_harvest = models.IntegerField()
