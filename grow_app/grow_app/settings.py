2797c2de697b0c6501084920a9e3565a6dd35c66
wstenzel <wills.stenzel@gmail.com>
2019-03-09 00:09:22 -0800

    Modified STATICFILES_DIRS and Secure cookies setting so that ft's would pass

--- grow_app/grow_app/settings.py
+++ grow_app/grow_app/settings.py
@@ -128,17 +128,12 @@ LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/logout/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-# The absolute path to the directory where collectstatic will collect static files for deployment.
+# # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
-
-# Secure Cookies
-CSRF_COOKIE_SECURE = True
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
