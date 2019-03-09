6b33e8cc1fecd3f807ee767a45a597770bb9e361
wstenzel <wills.stenzel@gmail.com>
2019-03-08 23:13:23 -0800

    Removed unneeded os import

--- grow_app/grow_app/settings.py
+++ grow_app/grow_app/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-import os
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
