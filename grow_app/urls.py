b27d14b8ea89efc0c38b6f259277432b789097ab
wstenzel <wills.stenzel@gmail.com>
2019-03-11 23:58:14 -0400

    refactored project structure to have the django project root (grow_app) in the main repo directory

--- grow_app/urls.py
+++ grow_app/urls.py
@@ -1,4 +1,4 @@
-"""grow_app URL Configuration
+"""grow_app_rm URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
--- grow_app/wsgi.py
+++ grow_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for grow_app project.
+WSGI config for grow_app_rm project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grow_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grow_app_rm.settings')
 
 application = get_wsgi_application()
