2a72d41ffabae2b1ff0aad955566ff71630dde89
wstenzel <wills.stenzel@gmail.com>
2019-03-13 11:55:24 -0400

    Fixed string parsing to int

--- grow_app/templates/homepage_demoController.js
+++ grow_app/templates/homepage_demoController.js
@@ -5,7 +5,7 @@
 
 document.addEventListener("barcode-scanned", function (e) {
     var barcode_str = e.detail
-    var parsed_barcode = barcode_str.parseInt() // # TODO -- Create a more robust barcode parsing function that maps barcodes to slot id's
+    var parsed_barcode = Number(barcode_str) // # TODO -- Create a more robust barcode parsing function that maps barcodes to slot id's
     window.location = "/slot/" + parsed_barcode + "/"
 });
 
