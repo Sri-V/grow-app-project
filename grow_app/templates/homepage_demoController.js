e911bd3e7458852c0dffc00fbce993b2bec1a767
wstenzel <wills.stenzel@gmail.com>
2019-03-13 11:51:24 -0400

    Added var before variable declarations

--- grow_app/templates/homepage_demoController.js
+++ grow_app/templates/homepage_demoController.js
@@ -4,8 +4,8 @@
 
 
 document.addEventListener("barcode-scanned", function (e) {
-    barcode_str = e.detail
-    parsed_barcode = barcode_str.parseInt() // # TODO -- Create a more robust barcode parsing function that maps barcodes to slot id's
+    var barcode_str = e.detail
+    var parsed_barcode = barcode_str.parseInt() // # TODO -- Create a more robust barcode parsing function that maps barcodes to slot id's
     window.location = "/slot/" + parsed_barcode + "/"
 });
 
