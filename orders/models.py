3694c9ebe2b259f3129b2ac7a40a25b9f8317536
Rebecca Mendez <35311987+rcmendez23@users.noreply.github.com>
2019-09-30 16:45:48 -0400

    Initialize models
    Account, Order, Product, TrayType, LiveCropProduct, HarvestedCropProduct

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from inventory.models import Variety
 
-# Create your models here.
+
+class Account(models.Model):
+    """Represents a customer of Boston Microgreens."""
+    name = models.CharField(max_length=200)
+    date_created = models.DateField(auto_now=True)
+    active = models.BooleanField()
+
+
+class Product(models.Model):
+    """Represents a type of product to be sold."""
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+
+
+class Order(models.Model):
+    """Represents an sales order of product."""
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    account = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    delivery_date = models.DateField()
+
+
+class TrayType(models.Model):
+    """Represents a type of growing tray."""
+    name = models.CharField(max_length=200)
+
+
+class LiveCropProduct(Product):
+    """Represents live tray microgreens products. Eg. 10 inch tray Sango"""
+    variety = models.ForeignKey(Variety, on_delete=models.CASCADE)
+    size = models.CharField(max_length=40, choices=['SMALL', 'LARGE'])
+    tray_type = models.ForeignKey(TrayType, on_delete=models.CASCADE)
+
+
+class HarvestedCropProduct(Product):
+    """Represents harvested microgreens products. Eg. 3oz clamshell of Shiso"""
+    variety = models.ForeignKey(Variety, on_delete=models.CASCADE)
+    size = models.CharField(max_length=40, choices=['SMALL', 'LARGE'])
+    weight = models.FloatField()
+
